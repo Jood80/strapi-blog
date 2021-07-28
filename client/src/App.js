865477a503fcdfc435385ac4ea4b234f53c320bc
Nujood <shawar.nujood@gmail.com>
2021-07-28 21:28:52 +0300

    Add Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-// import NavBar from './components/Navbar';
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
@@ -6,24 +6,30 @@ import Home from './pages/home';
 import Category from './pages/category';
 import BlogDetails from './pages/blogDetails';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route exact path='/' >
-            <Home/>
-          </Route>
-          <Route path='/details/:id' >
-            <BlogDetails/>
-          </Route>
-          <Route path='/category/:id' >
-            <Category/>
-          </Route>
-        </Switch>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' >
+              <Home/>
+            </Route>
+            <Route path='/details/:id' >
+              <BlogDetails/>
+            </Route>
+            <Route path='/category/:id' >
+              <Category/>
+            </Route>
+          </Switch>
+        </div>
+      </ApolloProvider>
     </Router>
   );
 }
