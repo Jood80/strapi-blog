7faad578f591ebb0a5877746ec64d134c931bc6f
Nujood <shawar.nujood@gmail.com>
2021-07-28 13:36:30 +0300

    Setup strapi

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '63c753902fcf38952cea4d79c1e2aa10'),
+    },
+  },
+});
