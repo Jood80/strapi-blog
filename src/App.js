17c4224d76ef3246c1b17eb017c1a5dcbf4e9a44
Nujood <shawar.nujood@gmail.com>
2021-07-28 17:52:40 +0300

    Add initial component & pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+// import NavBar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/home';
+import Category from './pages/category';
+import BlogDetails from './pages/blogDetails';
+
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path='/' >
+            <Home/>
+          </Route>
+          <Route path='/details/:id' >
+            <BlogDetails/>
+          </Route>
+          <Route path='/category/:id' >
+            <Category/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
